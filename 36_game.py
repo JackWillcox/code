fddf5e20c1f4e75aaa82ef57941df27765270b98
jacwil0809 <jackowillcox@gmail.com>
2022-05-05 13:04:01 +0100

    add lst of random nums

--- 36_game.py
+++ 36_game.py
@@ -1,6 +1,29 @@
-import tkinter
-#stuff more
+import tkinter as tk
+import random
+
 class ThirtySixGame:
 
     def __init__(self):
         pass
+
+    def all_possible_four_nums(self):
+        pass
+
+
+
+
+
+class Player:
+
+    def __init__(self, name):
+        pass
+
+    def apply_operation(self):
+        pass
+
+
+
+nums = [random.randint(1,15) for i in range(4)]
+def thirty_six_solver(nums):
+    pass
+
