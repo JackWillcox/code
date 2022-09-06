6f9d4821708fe0244629bd83bbd2cb92b4420f93
jacwil0809 <jackowillcox@gmail.com>
2022-09-06 12:48:10 +0100

    Links

--- connect4.py
+++ connect4.py
@@ -8,10 +8,17 @@ class Connect4:
         pass
 
 
+class Board:
+
+    def __init__(self):
+        pass
+
+
 class Red:
 
     def __init__(self):
         pass
+
 
 class Yellow:
 
@@ -21,3 +28,11 @@ class Yellow:
 
 def engine():
     pass
+
+
+# Useful Links
+
+# http://blog.gamesolver.org/solving-connect-four/01-introduction/
+# https://towardsdatascience.com/creating-the-perfect-connect-four-ai-bot-c165115557b0
+# https://tromp.github.io/c4/c4.html
+# https://github.com/lhorrell99/connect-4-solver/blob/master
